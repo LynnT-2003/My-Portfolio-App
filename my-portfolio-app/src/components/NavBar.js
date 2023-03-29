afe5b693fb12c7eca3b0e070c9ab3fc0100c195e
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2023-03-29 22:22:32 +0630

    first commit

---
+++ my-portfolio-app/src/components/NavBar.js
@@ -0,0 +1,90 @@
+import { useState, useEffect } from "react";
+import { NavBar, Container } from "react-bootstrap";
+
+export const NavBar = () => {
+  const [activeLink, setActiveLink] = useState("home");
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+  };
+
+  return (
+    <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
+      <Container>
+        <Navbar.Brand href="#home">
+          <img src={""} alt="logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <span className="navbar-toggle-icon"></span>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link
+              href="#home"
+              className={
+                activeLink === "home" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => {
+                onUpdateActiveLink("home");
+              }}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              href="#skills"
+              className={
+                activeLink === "skills" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => {
+                onUpdateActiveLink("skills");
+              }}
+            >
+              Skills
+            </Nav.Link>
+            <Nav.Link
+              href="#projects"
+              className={
+                activeLink === "projects" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => {
+                onUpdateActiveLink("projects");
+              }}
+            >
+              Projects
+            </Nav.Link>
+          </Nav>
+          <span classname="navbar-text">
+            <div classname="social-icon">
+              <a href="#">
+                <img src={""} alt="" />
+              </a>
+              <a href="#">
+                <img src={""} alt="" />
+              </a>
+              <a href="#">
+                <img src={""} alt="" />
+              </a>
+            </div>
+            <button className="vvd" onClick={() => console.log("connect")}>
+              <span>Let's Connect</span>
+            </button>
+          </span>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
