c975dae77ce0085298ad760da6eb1ea9c5abd56e
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2023-03-31 01:33:45 +0630

    added project links

--- my-portfolio-app/src/components/ProjectCard.js
+++ my-portfolio-app/src/components/ProjectCard.js
@@ -1,9 +1,12 @@
 import { Col } from "react-bootstrap";
 
-export const ProjectCard = ({ title, description, imgUrl }) => {
+export const ProjectCard = ({ title, description, imgUrl, projURL }) => {
   return (
     <Col size={12} sm={6} md={4}>
-      <div className="proj-imgbx">
+      <div
+        className="proj-imgbx"
+        onClick={() => window.open(projURL, "_blank")}
+      >
         <img src={imgUrl} />
         <div className="proj-txtx">
           <h4>{title}</h4>
