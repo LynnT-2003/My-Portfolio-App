fecf3407cf425ce55dc40887c2f27bc5bb012bd3
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2023-03-31 01:14:41 +0630

    added Projects component to App.js

--- my-portfolio-app/src/App.js
+++ my-portfolio-app/src/App.js
@@ -1,11 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useRef } from "react";
 import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
+import { Projects } from "./components/Projects";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const skillsRef = useRef(null);
   return (
     <div
       className="App"
@@ -22,6 +25,7 @@ function App() {
       <NavBar />
       <Banner />
       <Skills />
+      <Projects />
     </div>
   );
 }
