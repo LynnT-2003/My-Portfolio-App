a611c7458771926b693995948db27fa19f4a1ef6
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2023-03-30 22:14:33 +0630

    created Skills component

---
+++ my-portfolio-app/src/components/Skills.js
@@ -0,0 +1,74 @@
+import { Col, Row, Container } from "react-bootstrap";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import meter1 from "../assets/img/meter1.svg";
+import meter2 from "../assets/img/meter2.svg";
+import meter3 from "../assets/img/meter3.svg";
+import colorSharp from "../assets/img/color-sharp.png";
+
+export const Skills = () => {
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+
+  return (
+    <section className="skill" id="skills">
+      <Container>
+        <Row>
+          <Col>
+            <div className="skill-bx">
+              <h2>Skills</h2>
+              <p>
+                As a student with a passion for technology, I am proficient in
+                UI/UX design, Database Management, API development, and
+                Full-Stack Development. Constantly learning and improving my
+                abilities, keeping up to date with the latest industry standards
+                and technologies, I am confident that I can bring value to any
+                project or team.
+              </p>
+              <Carousel
+                responsive={responsive}
+                infinite={true}
+                className="skill-slider"
+              >
+                <div className="item">
+                  <img src={meter1} alt="image"></img>
+                  <h5>UI/UX Design</h5>
+                </div>
+                <div className="item">
+                  <img src={meter2} alt="image"></img>
+                  <h5>Database Management</h5>
+                </div>
+                <div className="item">
+                  <img src={meter3} alt="image"></img>
+                  <h5>API Development</h5>
+                </div>
+                <div className="item">
+                  <img src={meter1} alt="image"></img>
+                  <h5>Full-Stack Development</h5>
+                </div>
+              </Carousel>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-left" src={colorSharp} />
+    </section>
+  );
+};
