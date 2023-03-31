549dad5dc8d0845e1851e153111ab458cb9d69dd
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2023-03-31 18:16:17 +0630

    updated Projects

--- my-portfolio-app/src/components/ProjectCard.js
+++ my-portfolio-app/src/components/ProjectCard.js
@@ -2,12 +2,13 @@ import { Col } from "react-bootstrap";
 
 export const ProjectCard = ({ title, description, imgUrl, projURL }) => {
   return (
-    <Col size={12} sm={6} md={4}>
+    <Col size={12} sm={6} md="auto">
       <div
         className="proj-imgbx"
         onClick={() => window.open(projURL, "_blank")}
+        style={{ width: "30rem", height: "30rem" }}
       >
-        <img src={imgUrl} />
+        <img src={imgUrl} style={{ width: "100%", height: "100%" }} />
         <div className="proj-txtx">
           <h4>{title}</h4>
           <span>{description}</span>
--- my-portfolio-app/src/components/Projects.js
+++ my-portfolio-app/src/components/Projects.js
@@ -1,8 +1,8 @@
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
-import projImg1 from "../assets/img/grade-tracker.jpg";
-import projImg2 from "../assets/img/supplier.jpg";
-import projImg3 from "../assets/img/tutorPlus.png";
+import projImg1 from "../assets/img/gradetracker-logo.png";
+import projImg2 from "../assets/img/supplier-management-logo.png";
+import projImg3 from "../assets/img/tutorplus-logo.png";
 
 export const Projects = () => {
   const projects = [
@@ -45,23 +45,24 @@ export const Projects = () => {
               id="pills-tab"
             >
               <Nav.Item>
-                <Nav.Link eventKey="first">Tab One</Nav.Link>
+                <Nav.Link eventKey="first">Phase One</Nav.Link>
               </Nav.Item>
               <Nav.Item>
-                <Nav.Link eventKey="second">Tab Two</Nav.Link>
+                <Nav.Link eventKey="second">Phase Two</Nav.Link>
               </Nav.Item>
               <Nav.Item>
-                <Nav.Link eventKey="third">Tab Three</Nav.Link>
+                <Nav.Link eventKey="third">Phase Three</Nav.Link>
               </Nav.Item>
             </Nav>
             <Tab.Content>
               <Tab.Pane eventKey="first">
-                <Row>
+                <Row className="justify-content-center">
                   {projects.map((project, index) => {
                     return <ProjectCard key={index} {...project} />;
                   })}
                 </Row>
               </Tab.Pane>
+
               <Tab.Pane eventKey="second">
                 <p>Coming soon...</p>
               </Tab.Pane>
