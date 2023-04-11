1e98d20dc4cc29630affb7871db2dd30eaa8ee54
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2023-04-11 20:53:59 +0630

    Added new projects to portfolio

--- my-portfolio-app/src/components/Projects.js
+++ my-portfolio-app/src/components/Projects.js
@@ -3,9 +3,12 @@ import { ProjectCard } from "./ProjectCard";
 import projImg1 from "../assets/img/gradetracker-logo.png";
 import projImg2 from "../assets/img/supplier-management-logo.png";
 import projImg3 from "../assets/img/tutorplus-logo.png";
+import projImg4 from "../assets/img/ecommerce.png";
+import projImg5 from "../assets/img/discord-gpt-35-chatbot-logo.png";
+import projImg6 from "../assets/img/discord-translation-bot-logo.png";
 
 export const Projects = () => {
-  const projects = [
+  const projects_p1 = [
     {
       title: "Grade Tracker",
       description: "My First Ever Web Dev Project",
@@ -23,6 +26,30 @@ export const Projects = () => {
       description: "My First Full-Stack Team Project ",
       imgUrl: projImg3,
       projURL: "https://tutor-plus.vercel.app/",
+    },
+  ];
+
+  const projects_p2 = [
+    {
+      title: "Discord GPT-3.5 ChatBot",
+      description:
+        "(GitHub Repository) A Discord bot utilizing OpenAI's latest GPT-3.5-turbo API to generate friendly responses ",
+      imgUrl: projImg5,
+      projURL: "https://github.com/LynnT-2003/Discord-GPT-3.5-Bot",
+    },
+    {
+      title: "Discord Translation Bot",
+      description:
+        "(GitHub Repository) A Discord bot utilizing Google Translate's API to translate messages from English to Burmese ",
+      imgUrl: projImg6,
+      projURL: "https://github.com/LynnT-2003/Discord-Translate-Bot",
+    },
+    {
+      title: "E-commerce Application",
+      description:
+        "A Modern Full Stack E-Commerce Web Application built using React. Powered by Stripe payment system, and Sanity.io CMS",
+      imgUrl: projImg4,
+      projURL: "https://https://ecommerce-sanity-stripe-lynnt-2003.vercel.app/",
     },
   ];
 
@@ -57,14 +84,18 @@ export const Projects = () => {
             <Tab.Content style={{ paddingLeft: "1rem", paddingRight: "1rem" }}>
               <Tab.Pane eventKey="first">
                 <Row className="justify-content-center">
-                  {projects.map((project, index) => {
+                  {projects_p1.map((project, index) => {
                     return <ProjectCard key={index} {...project} />;
                   })}
                 </Row>
               </Tab.Pane>
 
               <Tab.Pane eventKey="second">
-                <p>Coming soon...</p>
+                <Row className="justify-content-center">
+                  {projects_p2.map((project, index) => {
+                    return <ProjectCard key={index} {...project} />;
+                  })}
+                </Row>
               </Tab.Pane>
               <Tab.Pane eventKey="third">
                 <p>Coming soon...</p>
